6e5a67deccfb824af6f56825906bc9403b5d0afb
hfisser <henrik.fisser@t-online.de>
2020-12-08 19:32:08 +0100

    swapped y and x.. was false before

--- vector_utils/geocoding.py
+++ vector_utils/geocoding.py
@@ -3,11 +3,6 @@ from shapely.geometry import Point
 
 
 def utm_to_4326(utm_point, src_crs, tgt_crs):
-    """
-    transforms an
-    :param utm_point:
-    :return:
-    """
     transformer = Transformer.from_crs(src_crs, tgt_crs)
     transformed = transformer.transform(utm_point[1], utm_point[0])
     return Point(transformed[1], transformed[0])
