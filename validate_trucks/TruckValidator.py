8bfca067ff82642a182a68282a4f6f4654e3a76a
hfisser <henrik.fisser@t-online.de>
2021-03-04 18:08:27 +0100

    series processing

--- validate_trucks/TruckValidator.py
+++ validate_trucks/TruckValidator.py
@@ -8,6 +8,7 @@ import pandas as pd
 from glob import glob
 from shapely.geometry import Point, box
 from fiona.errors import DriverError
+from datetime import date, timedelta
 
 from osm_utils.utils import get_roads
 from array_utils.geocoding import lat_from_meta, lon_from_meta
@@ -37,14 +38,14 @@ OSM_BUFFER = 20
 hour, minutes, year = 10, 15, 2018
 
 validate = "boxes"
-#validate = "bast"
+validate = "bast"
 
 stations = dict()
-stations_pd = pd.read_csv(os.path.join(os.path.dirname(aois_file), "validation_stations.csv"), sep=";")
-for station_name, start in zip(stations_pd["Name"], stations_pd["S2_date_start"]):
-    split = start.split(".")
-    date = "-".join([split[2], split[1], split[0]])
-    stations[station_name] = [date, date]
+bast_dir = os.path.dirname(aois_file)
+stations_pd = pd.read_csv(os.path.join(bast_dir, "validation_stations.csv"), sep=";")
+for station_name, start, end in zip(stations_pd["Name"], stations_pd["S2_date_start"], stations_pd["S2_date_end"]):
+    start, end = start.split("."), end.split(".")
+    stations[station_name] = ["-".join([split[2], split[1], split[0]]) for split in [start, end]]
 
 
 class Validator:
@@ -57,13 +58,13 @@ class Validator:
         for directory in self.dirs.values():
             if not os.path.exists(directory):
                 os.mkdir(directory)
-        self.validation_file = os.path.join(self.dirs["validation"], "validation_run.csv")
         self.station_name = station_denotation
         attribute_given = any(["(" + s + ")" in self.station_name for s in ["N", "E", "S", "W"]])
         self.station_name_clear = self.station_name.split(" (")[0]
         alternative_split = self.station_name.split(") (")[0].split(" (")[0]
         self.station_name_clear = alternative_split if attribute_given else self.station_name_clear
         self.station_name_clear = self.station_name_clear.replace(" ", "_")
+        self.validation_file = os.path.join(self.dirs["validation"], "validation_run_%s.csv" % self.station_name_clear)
         self.station_name = self.station_name.split("(1)")[0] if self.station_name.endswith("(1)") else self.station_name
         self.station_meta = self.get_station_meta(self.station_name)
         bbox = aois[aois["Name"] == self.station_name].geometry.bounds
@@ -72,66 +73,85 @@ class Validator:
         self.lat, self.lon = None, None
         self.detections, self.osm_roads = None, None
         self.date = None
-        self.detections_file, self.station_file, self.s2_data_file, = "", "", None
+        self.detections_file, self.s2_data_file, = "", None
 
-    def detect(self, period):
-        self.date = period[0]
-        band_names, resolution, folder = ["B04", "B03", "B02", "B08", "CLM"], 10, ""
-        dir_save_archive = os.path.join(self.dirs["s2"], "archive")
-        # keep this clean, only archive should be retained
-        for directory in glob(os.path.join(os.path.dirname(dir_save_archive), "*")):
-            if directory != dir_save_archive:
-                shutil.rmtree(directory)
-        if not os.path.exists(dir_save_archive):
-            os.mkdir(dir_save_archive)
-        sh = SentinelHub()
-        sh.set_credentials(SH_CREDENTIALS_FILE)
-        sh_bbox = (self.bbox_epsg4326[1], self.bbox_epsg4326[0], self.bbox_epsg4326[3], self.bbox_epsg4326[2])  # diff. order
-        merged_file = os.path.join(dir_save_archive, "s2_bands_%s_%s_%s_merged.tiff" % (self.station_name_clear,
-                                                                                        period[0],
-                                                                                        period[1]))
-        band_stack, folder = sh.get_data(sh_bbox, [date, date], DataCollection.SENTINEL2_L2A,
-                                         ["B08", "B04", "B03", "B02", "CLM"], resolution, self.dirs["s2"],
-                                         merged_file)
-        detections_file = os.path.join(self.dirs["detections"], "s2_detections_%s_%s.gpkg" %
-                                       (self.date, self.station_name_clear))
-        detector = RFTruckDetector()
-        band_stack = detector.read_bands(merged_file)
-        detector.preprocess_bands(band_stack)
-        detector.train()
-        prediction = detector.predict()
-        prediction_boxes = detector.extract_objects(prediction)
-        try:
-            detector.prediction_boxes_to_gpkg(prediction_boxes, detections_file)
-        except ValueError:
-            pass
-        station_folder = "zst" + self.station_name.split("(")[1].split(")")[0]
-        wrong = len(station_folder) == 4
-        station_folder = "zst" + self.station_name.split(") ")[1].split("(")[1][0:-1] if wrong else station_folder
-        self.station_file = os.path.join(self.dirs["station_counts"], station_folder, station_folder + "_%s.csv" %
-                                         str(year))
-        self.detections_file = detections_file
-        with rio.open(merged_file, "r") as src:
-            meta = src.meta
-        self.lat = lat_from_meta(meta)
-        self.lon = lon_from_meta(meta)
-        detector, band_stack_np = None, None
+    def validate_acquisition_wise(self, period):
+        dates_between = self.generate_process_periods(period)
+        for sub_period in dates_between:
+            self.date = sub_period[0]
+            band_names, resolution, folder = ["B04", "B03", "B02", "B08", "CLM"], 10, ""
+            dir_save_archive = os.path.join(self.dirs["s2"], "archive")
+            # keep this clean, only archive should be retained
+            for directory in glob(os.path.join(os.path.dirname(dir_save_archive), "*")):
+                if directory != dir_save_archive:
+                    shutil.rmtree(directory)
+            if not os.path.exists(dir_save_archive):
+                os.mkdir(dir_save_archive)
+            sh_bbox = (self.bbox_epsg4326[1], self.bbox_epsg4326[0], self.bbox_epsg4326[3], self.bbox_epsg4326[2])
+            merged_file = os.path.join(dir_save_archive, "s2_bands_%s_%s_%s_merged.tiff" % (self.station_name_clear,
+                                                                                            sub_period[0],
+                                                                                            sub_period[1]))
+            kwargs = dict(bbox=sh_bbox, period=sub_period, dataset=DataCollection.SENTINEL2_L2A,
+                          bands=["B04", "B03", "B02", "B08"], resolution=resolution, dir_save=self.dirs["s2"],
+                          merged_file=merged_file, mosaicking_order="leastCC")
+            sh = SentinelHub()
+            sh.set_credentials(SH_CREDENTIALS_FILE)
+            data_available = sh.data_available(kwargs)
+            if data_available:
+                print("Processing: %s" % sub_period[0])
+                kwargs_copy = kwargs.copy()
+                kwargs_copy["bands"] = ["CLM"]  # get cloud mask in order to check if low cloud coverage
+                kwargs_copy["merged_file"] = os.path.join(dir_save_archive, "clm.tiff")
+                clm, data_folder = sh.get_data(**kwargs_copy)  # get only cloud mask
+                has_obs = self.has_observations(kwargs_copy["merged_file"])
+                try:
+                    os.remove(kwargs_copy["merged_file"])  # cloud mask
+                except FileNotFoundError:
+                    pass
+                if has_obs:
+                    band_stack, folder = sh.get_data(**kwargs)  # get full data
+                    detections_file = os.path.join(self.dirs["detections"], "s2_detections_%s_%s.gpkg" %
+                                                   (self.date, self.station_name_clear))
+                    detector = RFTruckDetector()
+                    band_stack = detector.read_bands(merged_file)
+                    detector.preprocess_bands(band_stack)
+                    detector.mask_clouds(clm)
+                    detector.train()
+                    prediction = detector.predict()
+                    prediction_boxes = detector.extract_objects(prediction)
+                    try:
+                        detector.prediction_boxes_to_gpkg(prediction_boxes, detections_file)
+                    except ValueError:
+                        pass
+                    station_folder = "zst" + self.station_name.split("(")[1].split(")")[0]
+                    wrong = len(station_folder) == 4
+                    station_folder = "zst" + self.station_name.split(") ")[1].split("(")[1][0:-1] if wrong else station_folder
+                    station_file = os.path.join(self.dirs["station_counts"], station_folder, station_folder + "_%s.csv" %
+                                                str(year))
+                    self.detections_file = detections_file
+                    with rio.open(merged_file, "r") as src:
+                        meta = src.meta
+                    self.lat = lat_from_meta(meta)
+                    self.lon = lon_from_meta(meta)
+                    detector, band_stack_np = None, None
+                    # run comparison
+                    self.validate_with_bast(sub_period[0], detections_file, station_file, merged_file)
 
-    def validate_with_bast(self):
+    def validate_with_bast(self, acquisition_date, detections_file, station_file, s2_data_file):
         try:
             validation_pd = pd.read_csv(self.validation_file)
         except FileNotFoundError:
             validation_pd = pd.DataFrame()
         speed = 80
         try:
-            self.detections = gpd.read_file(self.detections_file)
+            self.detections = gpd.read_file(detections_file)
         except DriverError:
             self.detections = gpd.GeoDataFrame()
         else:
             self.prepare_s2_counts()
         hour_proportion = (minutes / 60)
         distance_traveled = hour_proportion * speed
-        station_counts = pd.read_csv(self.station_file, sep=";")
+        station_counts = pd.read_csv(station_file, sep=";")
         station_point = Point([self.station_meta["x"], self.station_meta["y"]])
         buffer_distance = distance_traveled * 1000
         station_buffer = station_point.buffer(buffer_distance)
@@ -161,18 +181,14 @@ class Validator:
                 s2_direction1 += 1
             elif match and heading < 180:
                 s2_direction2 += 1
-#        try:
- #           detections_in_reach = gpd.overlay(self.detections, station_buffer_gpd, "intersection")
-  #      except AttributeError:  # no detections in reach
-   #         detections_in_reach = gpd.GeoDataFrame()
         # compare number of detections in reach to the one of count station
         date_station_format = self.date[2:].replace("-", "")  # e.g. "2018-12-31" -> "181231"
         time_match = (station_counts["Datum"] == int(date_station_format)) * (station_counts["Stunde"] == hour)
         station_counts_hour = station_counts[time_match]
         idx = len(validation_pd)
-        for key, value in {"station_file": self.station_file, "s2_counts_file": self.s2_data_file,
-                           "detections_file": self.detections_file,
-                           "hour": hour, "n_minutes": minutes,
+        for key, value in {"station_file": station_file, "s2_counts_file": s2_data_file,
+                           "detections_file": detections_file,
+                           "date": acquisition_date, "hour": hour, "n_minutes": minutes,
                            "s2_direction1": s2_direction1, "s2_direction2": s2_direction2}.items():
             validation_pd.loc[idx, key] = [value]
         for column in station_counts_hour.columns[9:]:  # counts from station
@@ -226,6 +242,7 @@ class Validator:
             lens = np.int32([len(x) for x in imgs])
             img_file = imgs[np.where(lens == lens.max())[0]][0]
             name = os.path.basename(img_file).split(".tif")[0]
+            print(img_file)
             # read labels
             validation_boxes = gpd.read_file(os.path.join(dir_labels,
                                                           os.path.basename(img_file).split("_y0")[0] + ".gpkg"))
@@ -246,7 +263,8 @@ class Validator:
                 rf_td.train()
                 prediction_array = rf_td.predict()
                 prediction_boxes = rf_td.extract_objects(prediction_array)
-                rf_td.prediction_raster_to_gtiff(prediction_array, os.path.join(self.dirs["detections"], name + "_raster"))
+                rf_td.prediction_raster_to_gtiff(prediction_array,
+                                                 os.path.join(self.dirs["detections"], name + "_raster"))
                 rf_td.prediction_boxes_to_gpkg(prediction_boxes, prediction_boxes_file)
             producer_n, user_n = 0, 0
             percentage_intersection = []
@@ -350,22 +368,51 @@ class Validator:
                 out_arr[y, x] = int(np.count_nonzero(in_arr[y_low:y_up, x_low:x_up]) > 20)
         return out_arr
 
+    @staticmethod
+    def generate_process_periods(period_of_interest):
+        d0 = [int(part) for part in period_of_interest[0].split("-")]
+        d1 = [int(part) for part in period_of_interest[1].split("-")]
+        d0_date, d1_date = date(d0[0], d0[1], d0[2]), date(d1[0], d1[1], d1[2])
+        periods = []
+        for days in range((d1_date - d0_date).days):
+            d = (d0_date + timedelta(days)).strftime("%Y-%m-%d")
+            periods.append([d, d])
+        return periods
+
+    @staticmethod
+    def has_observations(cloud_mask_file):
+        min_valid_percentage = 95
+        rf_td = RFTruckDetector()
+        clm = rf_td.read_bands(cloud_mask_file)
+        clm[clm == 0] = 1  # nocloud=0
+        clm[clm == 255] = 0  # cloud=255
+        pseudo_band = np.random.random(len(clm.flatten())).reshape((clm.shape[0], clm.shape[1]))
+        pseudo_band_stack = np.float32([pseudo_band] * 4)  # run pseudo preprocessing due to OSM masking
+        rf_td.preprocess_bands(pseudo_band_stack)
+        n_valid = np.count_nonzero(~np.isnan(rf_td.variables[0]))  # n valid pixels masked to OSM roads
+        rf_td.mask_clouds(clm)
+        n_valid_masked = np.count_nonzero(~np.isnan(rf_td.variables[0]))  # n valid pixels cloud-masked
+        valid_percentage = (n_valid_masked / n_valid) * 100
+        rf_td, clm = None, None
+        return valid_percentage < min_valid_percentage
+
 
 if __name__ == "__main__":
-    try:
-        os.remove(os.path.join(dir_validation, "validation_run.csv"))
-    except FileNotFoundError:
-        pass
-    try:
-        os.remove(boxes_validation_file)
-    except FileNotFoundError:
-        pass
+    if validate == "bast":
+        try:
+            os.remove(os.path.join(dir_validation, "validation_run.csv"))
+        except FileNotFoundError:
+            pass
+    else:
+        try:
+            os.remove(boxes_validation_file)
+        except FileNotFoundError:
+            pass
     for station, acquisition_period in stations.items():
         validator = Validator(station, aois_file, dir_validation, dir_osm)
         if validate == "boxes":
             validator.validate_boxes()
             break
         else:
-            print("Station: %s" % station)
-            validator.detect(acquisition_period)
-            validator.validate_with_bast()
+            print("%s\nStation: %s" % ("-" * 50, station))
+            validator.validate_acquisition_wise(acquisition_period)
